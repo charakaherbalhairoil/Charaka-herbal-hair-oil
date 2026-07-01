fa534bfbe6a35562a76b6849a446d7e33a5a8354
charakaherbalhairoil <charakaherbalhairoil@gmail.com>
2026-07-02 01:28:33 +0530

    Add initial script for Charaka Herbal Hair Oil website

---
+++ script.js
@@ -0,0 +1,7 @@
+// Charaka Herbal Hair Oil Website
+
+console.log("Charaka Website Loaded Successfully!");
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Welcome to Charaka Herbal Hair Oil");
+});
